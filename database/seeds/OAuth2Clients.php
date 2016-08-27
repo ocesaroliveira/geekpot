d8b932750206483b406b146b992b8f3181122708
César Oliveira <cesar.o.almeida@gmail.com>
2016-08-27 19:31:31 -0300

    Insere um usuário admin no seed

--- database/seeds/OAuth2Clients.php
+++ database/seeds/OAuth2Clients.php
@@ -11,8 +11,9 @@ class OAuth2Clients extends Seeder
      */
     public function run()
     {
+        $email = 'admin@geekpot.com.br';
         $alreadyExists = DB::table('oauth_clients')
-            ->where('id', '=', 'admin')
+            ->where('id', '=', $email)
             ->count();
 
         if ($alreadyExists > 0) {
@@ -21,66 +22,44 @@ class OAuth2Clients extends Seeder
 
         $now = (new DateTime())->format('Y-m-d H:i:s');
 
+        $password = Hash::make('654321');
+        DB::table('users')
+            ->insert([
+                'name' => 'Admin',
+                'email' => $email,
+                'password' => $password,
+                'created_at' => $now,
+                'updated_at' => $now
+            ]);
+
+
         DB::table('oauth_clients')
             ->insert([
-                'id' => 'admin',
+                'id' => $email,
                 'secret' => '020105d25d9ff015368be6bc2988b2cc7b89cfe2',
                 'name' => 'Admin',
                 'created_at' => $now,
                 'updated_at' => $now,
             ]);
 
-        DB::table('oauth_client_scopes')
-            ->insert([
-                [
-                    'id' => 1,
-                    'scope_id' => 'create',
-                    'client_id' => 'admin',
+        $scopes = [
+            'create',
+            'read',
+            'update',
+            'delete',
+            'admin_list',
+            'admin_list_deleted',
+            'admin_manage'
+        ];
+
+        foreach ($scopes as $scope) {
+            DB::table('oauth_client_scopes')
+                ->insert([
+                    'scope_id' => $scope,
+                    'client_id' => $email,
                     'created_at' => $now,
                     'updated_at' => $now,
-                ],
-                [
-                    'id' => 2,
-                    'scope_id' => 'read',
-                    'client_id' => 'admin',
-                    'created_at' => $now,
-                    'updated_at' => $now,
-                ],
-                [
-                    'id' => 3,
-                    'scope_id' => 'update',
-                    'client_id' => 'admin',
-                    'created_at' => $now,
-                    'updated_at' => $now,
-                ],
-                [
-                    'id' => 4,
-                    'scope_id' => 'delete',
-                    'client_id' => 'admin',
-                    'created_at' => $now,
-                    'updated_at' => $now,
-                ],
-                [
-                    'id' => 5,
-                    'scope_id' => 'admin_list',
-                    'client_id' => 'admin',
-                    'created_at' => $now,
-                    'updated_at' => $now,
-                ],
-                [
-                    'id' => 6,
-                    'scope_id' => 'admin_list_deleted',
-                    'client_id' => 'admin',
-                    'created_at' => $now,
-                    'updated_at' => $now,
-                ],
-                [
-                    'id' => 7,
-                    'scope_id' => 'admin_manage',
-                    'client_id' => 'admin',
-                    'created_at' => $now,
-                    'updated_at' => $now,
-                ]
-            ]);
+                ]);
+        }
     }
 }
