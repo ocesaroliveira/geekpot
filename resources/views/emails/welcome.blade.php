317adfa0544fc1c127793deff2911986d3ef4184
César Oliveira <cesar.o.almeida@gmail.com>
2016-08-27 20:50:30 -0300

    Template de e-mail de cadastro

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,18 @@
+Olá!
+<br>
+Você se cadastrou na nossa API da GeekPot e estamos muito felizes em contar com você!
+Seus dados de acesso via OAuth2 na nossa API são:
+<br>
+<b>name:</b> {{ $oauthClientData['name'] }}
+<br>
+<b>key:</b> {{ $oauthClientData['id'] }}
+<br>
+<b>secret:</b> {{ $oauthClientData['secret'] }}
+<br>
+<br>
+Aguardamos os seus requests!
+<br>
+<br>
+Atenciosamente,
+<br>
+GeekPot IT
